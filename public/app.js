396140a54a63f4a5d45edeaa733f37391c0ecfdc
chilleno <antonion.gonzalezf@gmail.com>
2024-02-04 18:34:34 -0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -15,7 +15,6 @@ function onYouTubeIframeAPIReady() {
     });
 }
 
-// 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
     console.log('ready')
     logProgress()
@@ -27,9 +26,6 @@ function logProgress() {
     console.log(player.data)
 }
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
 var done = false;
 var videoProgress = 0;
 
