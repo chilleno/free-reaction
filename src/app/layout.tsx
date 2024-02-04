512a9d641996d97ee839ad85dd6c5cb6bddf2a25
chilleno <antonion.gonzalezf@gmail.com>
2024-02-04 18:33:50 -0300

    feat: import youtube script and needed functions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        src="https://www.youtube.com/iframe_api"
+      />
+      <Script
+        src="./app.js" />
+
       <body className={inter.className}>{children}</body>
     </html>
   );
