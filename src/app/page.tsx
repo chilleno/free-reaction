a2cfd803f9349429972a767d24f3971dae1f3b78
chilleno <antonion.gonzalezf@gmail.com>
2024-02-04 19:51:44 -0300

    feat: trigger reaction start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 "use client"
-import {useState} from "react";
+import { useState } from "react";
 
 export default function Home() {
   const [volume, setVolume] = useState(100);
 
-  const changeVolume = (e:any) => {
+  const changeVolume = (e: any) => {
     setVolume(e.target.value);
   }
 
@@ -16,38 +16,35 @@ export default function Home() {
         width="640"
         height="600"
       />
-      <div className="flex flex-row gap-5 items-center">
-        <button
-          id="play"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Play
-        </button>
-        <button
-          id="pause"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Pause
-        </button>
-        <button
-          id="stop"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Stop
-        </button>
-        <button
-          id="mute"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Mute
-        </button>
+      <div id="reaction_control_buttons" className="flex flex-row gap-5 items-center hidden">
+        <div className="flex gap-5">
+          <button
+            id="play_button_reaction"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hidden"
+          >
+            Play
+          </button>
+          <button
+            id="pause_button_reaction"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Pause
+          </button>
+        </div>
+
         <div
           id="volume"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex gap-2"
-         
         >
           <input type="range" value={volume} onChange={changeVolume} />
-          <input id="volume_value_input" type="number" className="text-black max-w-14 text-right" value={volume} onChange={(e) => e.preventDefault} />
+          <input id="volume_value_input" type="number" className="text-black max-w-14 text-right rounded-sm" value={volume} readOnly onChange={(e) => e.preventDefault} />
+        </div>
+        <div
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex gap-2"
+        >
+          <h6 id="reaction_current_time_formatted">0:00</h6>
+          <input id="reaction_current_time" type="number" className="text-black max-w-14 text-right rounded-sm hidden" />
+          <input id="reaction_start_at" defaultValue={433.938901} type="number" className="text-black max-w-14 text-right rounded-sm hidden" />
         </div>
       </div>
     </main>
