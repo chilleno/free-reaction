567f446c40f5b8deb45733e82a3e835ef5ca0979
chilleno <antonion.gonzalezf@gmail.com>
2024-02-16 17:51:23 -0300

    feat: new page reactions

--- src/app/reactions/page.tsx
+++ src/app/reactions/page.tsx
@@ -1,13 +1,22 @@
+"use client"
+import { useState } from 'react'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
-
+import NewReaction from '@/components/Reactions/NewReaction'
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
+  const [openModal, setOpenModal] = useState(false);
+  const { data: session, status } = useSession();
+  let uuid = session?.user?.id;
+
+  if (!uuid) return 'Loading...';
+  
   return (
     <>
       <Header />
       <main>
-          
+          <NewReaction userId={uuid} openModal={openModal} />
       </main>
       <Footer />
     </>
