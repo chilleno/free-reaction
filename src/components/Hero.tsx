a98d3c0ca912ac28f7cca47df9dd25cea6adb159
chilleno <antonion.gonzalezf@gmail.com>
2024-02-16 12:59:20 -0300

    feat: add new links

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -31,18 +31,26 @@ export function Hero() {
         Discover a platform designed for content creators who value integrity and originality. Explore Reaction Free and unleash your creativity <span className="text-[#2f6b2f] underline underline-offset-2 font-bold">without fear of copyright infringement.</span>
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
-        <Button href="/login">start for free</Button>
+        <Button href="/login">start as a creator</Button>
         <Button
-          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          href="https://chromewebstore.google.com/detail/free-reactions/dnjfoljmledpdphbilpfcpmgjmcnbiic?hl=en"
           variant="outline"
+          target="_blank"
         >
-          <svg
-            aria-hidden="true"
-            className="h-3 w-3 flex-none fill-[#2f6b2f] group-active:fill-current"
-          >
-            <path d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z" />
+          <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-3 w-3 flex-none fill-[#2f6b2f] group-active:fill-current">
+            <path stroke-linecap="round" strokeLinejoin="round" d="M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 0 1-.657.643 48.39 48.39 0 0 1-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 0 1-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 0 0-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 0 1-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 0 0 .657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 0 1-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 0 0 5.427-.63 48.05 48.05 0 0 0 .582-4.717.532.532 0 0 0-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 0 0 .658-.663 48.422 48.422 0 0 0-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 0 1-.61-.58v0Z" />
           </svg>
-          <span className="ml-3">Watch video</span>
+          <span className="ml-3">Download the extension</span>
+        </Button>
+        <Button
+          href="https://youtu.be/1y7ww-seaPA"
+          variant="outline"
+          target="_blank"
+        >
+          <svg aria-hidden="true" className="h-3 w-3 flex-none fill-[#2f6b2f] group-active:fill-current">
+            <path d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z"></path>
+          </svg>
+          <span className="ml-3">Watch the demo video</span>
         </Button>
       </div>
       <div className="mt-36 lg:mt-44">
