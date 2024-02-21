7ab0d05278c1ceb4b5cd7d07db7922d9f2ab10a4
chilleno <antonion.gonzalezf@gmail.com>
2024-02-21 16:24:06 -0300

    feat: add product hunt badge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
           /* commented like this to keep the order */
           /* <SecondaryFeatures /> */
         }
-      
+
         {
           /* commented like this to keep the order */
           /* <Testimonials /> */
@@ -27,6 +27,9 @@ export default function Home() {
         <Pricing />
         <Faqs />
         <CallToAction />
+        <div className="fixed bottom-4 left-4 flex justify-center items-center cursor-pointer z-40">
+          <a href="https://www.producthunt.com/posts/reaction-free?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-reaction&#0045;free" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=440836&theme=light" alt="Reaction&#0032;Free - Create&#0032;react&#0032;content&#0032;without&#0032;fear&#0032;of&#0032;copyright&#0032;infringement&#0046; | Product Hunt" style={{ width: '250px', height: '54px' }} width="250" height="54" /></a>
+        </div>
       </main>
       <Footer />
     </>
